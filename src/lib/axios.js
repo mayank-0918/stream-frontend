384327886bd96f1f21c9983210558a5adaf32e73
Mayank kshirsagar <mayank9180@gmail.com>
2025-05-06 19:03:35 +0530

    change url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -7,13 +7,8 @@
 //   withCredentials: true, // send cookies with the request
 // });
 
-
-import axios from "axios";
-
-// Use environment variable for the base URL
-const BASE_URL = import.meta.env.VITE_STREAM_URL;
-
-export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+import axios from "axios"
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_STREAM_URL,
+  withCredentials: true,
 });
