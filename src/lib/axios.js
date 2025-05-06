ccd3019c9c5ebae9ad0afaa385f54297c5de286b
Mayank kshirsagar <mayank9180@gmail.com>
2025-05-06 19:05:13 +0530

    changing

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -8,7 +8,7 @@
 // });
 
 import axios from "axios"
-const axiosInstance = axios.create({
+ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_STREAM_URL,
   withCredentials: true,
 });
