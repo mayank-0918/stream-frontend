8be3641fdfcc6af0137904065c38ab80e3f7e259
Mayank kshirsagar <mayank9180@gmail.com>
2025-05-06 18:48:05 +0530

    update backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,17 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// export const axiosInstance = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true, // send cookies with the request
+// });
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// Use environment variable for the base URL
+const BASE_URL = import.meta.env.VITE_STREAM_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
