352de1b52a98ca96ce635cd7caa76c7e1240ad60
Mayank kshirsagar <mayank9180@gmail.com>
2025-05-08 15:39:14 +0530

    done

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -10,7 +10,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_STREAM_URL,
+  baseURL: import.meta.env.VITE_STREAM_URL || "http://localhost:5001/api",
   withCredentials: true,  // Ensure cookies are sent with requests
   headers: {
     'Accept': 'application/json',
